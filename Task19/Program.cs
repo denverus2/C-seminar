ea912dd31dfde9f3627c0c90e1835af3ca3995ae
Denverus <61493273+denverus2@users.noreply.github.com>
2023-06-17 14:09:30 +0300

    add HomeWork 19 21 23

---
+++ Task19/Program.cs
@@ -0,0 +1,28 @@
+﻿// Задача 19
+// Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+// 14212 -> нет
+// 12821 -> да
+// 23432 -> да
+
+
+
+
+Console.Write("Введите пятизначное число ");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number>9999&&number<100000)
+{
+  if (number/10000==number%10 && ((number/1000)%10)==(number/10)%10)
+  {
+    System.Console.WriteLine($"Число {number} полиндром");
+  }
+  else
+  {
+    System.Console.WriteLine($"Число {number} не полиндром");
+  }
+
+}
+
+else
+{
+  System.Console.WriteLine("Обычно в пятизначном числе 5 цифр. Давайте примем это за аксиому");
+}
---
+++ Task20/Program.cs
@@ -0,0 +1,21 @@
+﻿// Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 2D пространстве.
+
+// A (3,6); B (2,1) -> 5,09 
+// A (7,-5); B (1,-1) -> 7,21
+
+
+// AB = √(xb - xa)2 + (yb - ya)2
+
+Console.Write("Введите x первой точки ");
+int x1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите y первой точки ");
+int y1 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите x второй точки ");
+int x2 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите y второй точки ");
+int y2 = Convert.ToInt32(Console.ReadLine());
+
+double ab = Math.Round (Math.Sqrt(Math.Pow((x2-x1),2)+Math.Pow((y2-y1),2)),2);
+
+System.Console.WriteLine(ab);
