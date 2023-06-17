c60ad6f6903c13830ab4f0ae7373ad3165b08226
Denverus <61493273+denverus2@users.noreply.github.com>
2023-06-17 13:49:36 +0300

    fix number

--- Task21/Program.cs
+++ Task21/Program.cs
@@ -1,14 +1,21 @@
-﻿// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу квадратов чисел 
-// от 1 до N.
+﻿// Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 2D пространстве.
 
-// 5 -> 1, 4, 9, 16, 25.
-// 2 -> 1,4
+// A (3,6); B (2,1) -> 5,09 
+// A (7,-5); B (1,-1) -> 7,21
 
-Console.Write("Введите x  ");
-int x = Convert.ToInt32(Console.ReadLine());
-int i=1;
-while (i<=x)
-{
-  System.Console.Write($"{Math.Pow(i,2)}, ");
-  i++;
-}
+
+// AB = √(xb - xa)2 + (yb - ya)2
+
+Console.Write("Введите x первой точки ");
+int x1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите y первой точки ");
+int y1 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите x второй точки ");
+int x2 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите y второй точки ");
+int y2 = Convert.ToInt32(Console.ReadLine());
+
+double ab = Math.Round (Math.Sqrt(Math.Pow((x2-x1),2)+Math.Pow((y2-y1),2)),2);
+
+System.Console.WriteLine(ab);
