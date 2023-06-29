8bd1faaf38d3701be2f53ddfb53eb4a19edc5e9b
Denverus <61493273+denverus2@users.noreply.github.com>
2023-06-29 17:50:57 +0300

    Add HomeWork  41 43

---
+++ Task41/Program.cs
@@ -0,0 +1,36 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 3
+
+int[] FillArr(int m)
+{
+
+  int[] array = new int[m];
+  for (int i = 0; i < array.Length; i++)
+  {
+    System.Console.Write($"{i + 1}: ");
+    array[i] = Convert.ToInt32(Console.ReadLine());
+  }
+  return array;
+}
+int IsPositive(int[] arr)
+{
+  int count = 0;
+  for (int i = 0; i < arr.Length - 1; i++)
+  {
+    if (arr[i] > 0)
+    {
+      count++;
+    }
+
+
+  }
+  return count;
+}
+
+
+
+System.Console.WriteLine("Сколько чисел вводим?:");
+int m = Convert.ToInt32(Console.ReadLine());
+int[] arr = FillArr(m);
+System.Console.WriteLine($"Чисел больше нуля: {IsPositive(arr)}");
---
+++ Task43/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+
+// b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+
+double[] PointOfTouch(double b1, double k1, double b2, double k2)
+
+{
+  double[] arr = new double[2];
+  arr[0] = (b2 - b1) / (k1 - k2);
+  arr[1] = k1 * arr[0] + b1;
+  return arr;
+}
+
+System.Console.WriteLine("y = k1 * x + b1, y = k2 * x + b2");
+System.Console.WriteLine("Введите b1:");
+double b1 = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine("Введите k1:");
+double k1 = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine("Введите b2:");
+double b2 = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine("Введите k2:");
+double k2 = Convert.ToInt32(Console.ReadLine());
+
+System.Console.WriteLine(string.Join(",", PointOfTouch(b1, k1, b2, k2)));
